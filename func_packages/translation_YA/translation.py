0a508c4b9ad96967099ed9409d2f2d18dea4d966
Aligruit@gmail.com <Aligruit@gmail.com>
2020-06-07 17:56:43 +0300

    +fixing functions

--- func_packages/translation_YA/translation.py
+++ func_packages/translation_YA/translation.py
@@ -7,7 +7,10 @@ url = "https://translate.yandex.net/api/v1.5/tr.json/translate"
 def get_translate(text, lang):
     TEXT = text
     LANG = lang
-    request_post = requests.post(url, data={'key': token, 'text': TEXT, 'lang': LANG})
-    response_data = request_post.json()
-
-    return response_data
+    try:
+        request_post = requests.post(url, data={'key': token, 'text': TEXT, 'lang': LANG})
+        response_data = request_post.json()
+        return response_data
+    except Exception as error:
+        print(error)
+        return error
--- func_packages/wiki_func/wikipedia_help.py
+++ func_packages/wiki_func/wikipedia_help.py
@@ -2,5 +2,9 @@ from conf_packages.settings_for_all import *
 
 
 def getting_link(text_from_user):
-    page_py = wiki_wiki.page(text_from_user.lower())
-    return page_py
+    try:
+        page_py = wiki_wiki.page(text_from_user.lower())
+        return page_py
+    except Exception as error:
+        print(error)
+        return error
