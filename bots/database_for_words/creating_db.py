94960a905580f4ce7938880893ea2793481d540e
Ilya Erlingas <Aligruit@gmail.com>
2020-07-17 09:25:14 +0300

    creation of data base

--- bots/database_for_words/creating_db.py
+++ bots/database_for_words/creating_db.py
@@ -1,3 +1,18 @@
-import psyqo2
-cursor=
+import psycopg2
+connection = psycopg2.connect(database='main_database',
+            user='helper_io',
+            password=f'{}',
+            host='127.0.0.1',
+            port='5432',
+            )
+cursor=connection.cursor()
 def creating_local():
+    try:
+        cursor.execute('''CREATE TABLE USERS
+        (ID SERIAL PRIMARY KEY NOT NULL,
+        USER_NAME TEXT NOT NULL
+        WORDS_TO_TRANSLATE TEXT NOT NULL
+        TRANSLATED_WORDS TEXT NOT NULL)'''
+        )
+    except Exception as error:
+        print('There is an exception, database already excits!')
